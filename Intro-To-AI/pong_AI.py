7b1a7a12c33f8c56a0bfe6c76e371747878007dd
lwgover <lgover@pugetsound.edu>
2023-07-12 08:47:17 -0500

    added Intro-To-AI

---
+++ Intro-To-AI/pong_AI.py
@@ -0,0 +1,32 @@
+import pygame
+RED = (255, 0,0)
+class Paddle:
+    COLOR = RED
+    VEL = 4
+
+    def __init__(self, x, y, width, height):
+        self.x = self.original_x = x
+        self.y = self.original_y = y
+        self.width = width
+        self.height = height
+
+    def draw(self, win):
+        pygame.draw.rect(
+            win, self.COLOR, (self.x, self.y, self.width, self.height))
+
+    def move(self, up=True):
+        if up:
+            self.y -= self.VEL
+        else:
+            self.y += self.VEL
+
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+
+class AI_Paddle(Paddle):
+    def ai_move(self, ball):
+        #ball.x returns the ball's x coordinate
+        #ball.y returns the ball's y coordinate
+
+        return
